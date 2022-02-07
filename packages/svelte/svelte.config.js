a9d3083e9a117f7b7a1833575db93cfd2330221a
Doug A <douganderson444@gmail.com>
2022-02-07 11:16:39 -0400

    add gh pages, switch to static adapter

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,17 +1,61 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
+
+console.log({ dev });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({}),
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			pages: '../../docs',
+			assets: '../../docs'
+		}),
+		paths: {
+			// import.meta.env.DEV // doesn't work :/
+			// process.env.NODE_ENV // doesn't work :/
+			base: dev ? '' : '/iframe-wallet-engine'
+		},
+		vite: {
+			build: {
+				rollupOptions: {
+					// https://rollupjs.org/guide/en/#big-list-of-options
+					output: {
+						minifyInternalExports: false,
+						compact: false
+					},
+					plugins: []
+				},
+				minify: false,
+				sourcemap: true,
+				optimization: {
+					minimize: false
+				}
+			},
+			define: {
+				'process.env': process.env
+			},
+			optimization: {
+				minimize: false
+			},
+			resolve: {
+				alias: {}
+			},
+			commonjsOptions: {},
+			server: {
+				fs: {
+					// Allow serving files from levels up to the project root
+					allow: ['../', '../../', '../../../']
+				}
+			}
+			// plugins: [] // vite plugins
+		}
 	}
 };
 
