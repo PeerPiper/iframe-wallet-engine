d242389c97cfa8db0f40a5b2268423c213f8cdbb
Doug A <douganderson444@gmail.com>
2022-02-18 09:00:41 -0400

    add: config confirm components

--- packages/svelte/src/lib/components/confirm/index.ts
+++ packages/svelte/src/lib/components/confirm/index.ts
@@ -2,6 +2,8 @@ import { handlers } from '@peerpiper/iframe-wallet-engine';
 import DefaultConfirmation from './DefaultConfirmation.svelte';
 import Connect from './Connect.svelte';
 // import ArweaveSign from '../arweave/ConfirmSign.svelte';
+import Sign from './Sign.svelte';
+import Decrypt from './Decrypt.svelte';
 
 let confirmationComponents = {
 	Default: { component: DefaultConfirmation }
@@ -16,11 +18,18 @@ for (const [key, value] of Object.entries(handlers)) {
 let customizedComponents = {
 	connect: { component: Connect },
 	ed25519: {
-		// sign: {}
+		sign: {
+			component: Sign
+		}
 	},
 	arweaveWalletAPI: {
 		sign: {
 			// component: ArweaveSign
+		}
+	},
+	proxcryptor: {
+		selfDecrypt: {
+			component: Decrypt
 		}
 	}
 };
